450a97063f9bf98e24a8d284eef4598f720eb4df
spfaith <spfaith@gmail.com>
2016-09-27 00:24:40 +0900

    additional wild card commit

--- 01_Algorithm/src/wildcard/WildCardKYT.java
+++ 01_Algorithm/src/wildcard/WildCardKYT.java
@@ -4,13 +4,13 @@ import java.io.*;
 import java.util.*;
 
 public class WildCardKYT {
-    static int testCase;
+    static int C;
     static int N;
-    static String wildStr;
+    static String W;
     static String[] normalStrArr;
     static LinkedList<String>[] res;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = null;
     	
     	try {
@@ -18,39 +18,46 @@ public class WildCardKYT {
     	} catch(FileNotFoundException e) {
     		br = new BufferedReader(new InputStreamReader(System.in));
     	}
-        testCase = Integer.parseInt(br.readLine());
-        res = new LinkedList[testCase];
+    	// 첫번째 줄 : Test Case의 수 C
+        C = Integer.parseInt(br.readLine());
+        // LinkedList 생성
+        res = new LinkedList[C];
         
-        for(int t = 0; t < testCase; t++) {
-            wildStr = br.readLine();
+        for (int t = 0; t < C; t++) {
+        	// 두번째 줄 : Wildcard pattern W
+            W = br.readLine();
+            
+            // 세번째 줄 : 파일명의 수 n
             N = Integer.parseInt(br.readLine().trim());
             
             res[t] = new LinkedList<>();
             normalStrArr = new String[N];
             
-            for(int n = 0; n < N; n++) {
+            // 
+            for (int n = 0; n < N; n++) {
                 normalStrArr[n] = br.readLine();
-                if(isMatched(wildStr.toCharArray(), 0,
-                             normalStrArr[n].toCharArray(), 0))
-                    res[t].add(normalStrArr[n]);
+                if (isMatched(W.toCharArray(), 0, normalStrArr[n].toCharArray(), 0)) {
+                	res[t].add(normalStrArr[n]);
+                }
             }
         }
         
-        for(LinkedList<String> resRow : res) {
+        // 주어진 패턴에 대응되는 파일들의 이름을 한 줄에 하나씩 알파벳 순서대로 출력
+        for (LinkedList<String> resRow : res) {
             Collections.sort(resRow);
-            for(String r : resRow)
+            for (String r : resRow) {
                 System.out.println(r);
+            }
         }
     }
     
-    static boolean isMatched(char[] wild, int i,
-                             char[] normal, int j) {
-        final int wildLen = wild.length;
+    static boolean isMatched(char[] wildcard, int i, char[] normal, int j) {
+        final int wildLen = wildcard.length;
         final int normalLen = normal.length;
         
         while(i < wildLen && j < normalLen) {
-            int c = wild[i++];
-            switch(c) {
+            int c = wildcard[i++];
+            switch (c) {
                 case '?' :
                     j++;
                     break;
@@ -59,38 +66,45 @@ public class WildCardKYT {
                         j = normalLen;
                         break;
                     }
+                    // wildcard 는 여러개의 문자와 매칭되므로 다음 wildcard가 나오거나 문자의 끝이 나올때까지 List로 저장
                     List<Character> token = new ArrayList<>();
                     while(i < wildLen
-                            && wild[i] != '?'
-                            && wild[i] != '*')
-                        token.add(wild[i++]);
+                            && wildcard[i] != '?'
+                            && wildcard[i] != '*') {
+                        token.add(wildcard[i++]);
+                    }
+                    
                     List<Integer> idxList = getIndex(normal, j, token);
-                    for(int d = idxList.size() - 1; d >= 0; d--)
-                        if(isMatched(wild, i, normal,
-                                     idxList.get(d) + token.size()))
-                            return true;
+                    for (int d = idxList.size() - 1; d >= 0; d--) {
+                        return isMatched(wildcard, i, normal,
+                                     idxList.get(d) + token.size());
+                    }
                     return false;
                 default :
-                    if(c == normal[j])
+                    if (c == normal[j]) {
                         j++;
-                    else
+                    } else {
                         return false;
+                    }
+                    break;
             }
         }
         
-        return (i == wildLen - 1 && wild[i] == '*')
+        return (i == wildLen - 1 && wildcard[i] == '*')
                 || (i == wildLen && j == normalLen);
     }
     
-    static List<Integer> getIndex(char[] normal, int j, 
-                                  List<Character> token) {
+    static List<Integer> getIndex(char[] normal, int j, List<Character> token) {
         List<Integer> idxList = new ArrayList<>();
         LOOP :
-        for(int k = j; k < normal.length - token.size() + 1; k++) {
-            for(int m = 0; m < token.size(); m++)
-                if(normal[k + m] != token.get(m))
+        for (int k = j; k < normal.length - token.size() + 1; k++) {
+            for (int m = 0; m < token.size(); m++) {
+                if (normal[k + m] != token.get(m)) {
                     continue LOOP;
-            idxList.add(k);
+                }
+                idxList.add(k);
+            }
+            
         }
         return idxList;
     }
